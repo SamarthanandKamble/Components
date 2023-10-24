b4ab93a902fef1f32bcf518db44d21498e4a5158
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-10-24 18:00:47 +0530

    Started with Menu, added side menu with onclick functionalities

---
+++ Menu/script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector("#ul-menu");
+let menuIcon = document.querySelector("#menu-logo");
+let paras = document.querySelectorAll(".paras");
+let isMenuOpen = false;
+
+menuIcon.addEventListener("click", () => {
+  if (isMenuOpen) {
+    menu.style.display = "none";
+    menu.style.backgroundColor = "#000";
+    menu.style.backgroundColor = 1;
+    paras.forEach((e) => (e.style.opacity = 1));
+    isMenuOpen = false;
+  } else {
+    menu.style.display = "block";
+    menu.style.backgroundColor = "#000";
+    menu.style.backgroundColor = 1;
+    paras.forEach((e) => (e.style.opacity = 0.2));
+    isMenuOpen = true;
+  }
+  console.log(isMenuOpen);
+});
