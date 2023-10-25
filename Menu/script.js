f28f4294f575a9c36a17427694f4fac60435187c
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-10-25 11:58:16 +0530

    Resolved opacity bug

--- Menu/script.js
+++ Menu/script.js
@@ -7,13 +7,13 @@ menuIcon.addEventListener("click", () => {
   if (isMenuOpen) {
     menu.style.display = "none";
     menu.style.backgroundColor = "#000";
-    menu.style.backgroundColor = 1;
+    menu.style.opacity = 0;
     paras.forEach((e) => (e.style.opacity = 1));
     isMenuOpen = false;
   } else {
     menu.style.display = "block";
     menu.style.backgroundColor = "#000";
-    menu.style.backgroundColor = 1;
+    menu.style.opacity = 1;
     paras.forEach((e) => (e.style.opacity = 0.2));
     isMenuOpen = true;
   }
