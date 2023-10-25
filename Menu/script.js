a39cc9dd95b9d937a4106f099ecba98a444fee66
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-10-25 12:20:38 +0530

    Added sub menu

--- Menu/script.js
+++ Menu/script.js
@@ -1,7 +1,10 @@
 let menu = document.querySelector("#ul-menu");
 let menuIcon = document.querySelector("#menu-logo");
 let paras = document.querySelectorAll(".paras");
+let subMenuCnt = document.querySelector("#submenu-1-container");
+let subMenuCntIcon = document.querySelector("#sub-menu-cnt-icon");
 let isMenuOpen = false;
+let isSubMenuOpen = false;
 
 menuIcon.addEventListener("click", () => {
   if (isMenuOpen) {
@@ -10,12 +13,24 @@ menuIcon.addEventListener("click", () => {
     menu.style.opacity = 0;
     paras.forEach((e) => (e.style.opacity = 1));
     isMenuOpen = false;
+    isSubMenuOpen = false;
+    subMenuCnt.style.display = "none";
   } else {
     menu.style.display = "block";
+    menu.style.zIndex = 1;
     menu.style.backgroundColor = "#000";
     menu.style.opacity = 1;
     paras.forEach((e) => (e.style.opacity = 0.2));
     isMenuOpen = true;
   }
-  console.log(isMenuOpen);
+});
+
+subMenuCntIcon.addEventListener("click", () => {
+  if (isSubMenuOpen) {
+    subMenuCnt.style.display = "none";
+    isSubMenuOpen = false;
+  } else {
+    subMenuCnt.style.display = "block";
+    isSubMenuOpen = true;
+  }
 });
